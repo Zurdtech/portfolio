2a1c6cc3ab8ab3936a9eb2ad19bd93b53d9c0158
Zurdtech <adexsade8@gmail.com>
2025-03-23 13:03:15 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// const toggle = document
+//   .querySelector(".menu-box")
+//   .addEventListener("click", function (e) {
+//     document.querySelector(".body").classList.toggle("toggle");
+//   });
+
+
+const aside = document.querySelector(".toggle");
+const open = document.querySelector(".open");
+const close = document.querySelector(".close");
+const asideList = document.querySelectorAll(".name");
+
+open.addEventListener("click", function (e) {
+  aside.classList.toggle("toggle");
+});
+close.addEventListener("click", function (e) {
+  aside.classList.toggle("toggle");
+});
+
+function asideAction(){
+  asideList.forEach(s => s.classList.remove('active2'))
+  this.classList.add('active2')
+  aside.classList.toggle('toggle')
+}
+
+asideList.forEach(s => s.addEventListener('click', asideAction))
+
+
+// icons.forEach(s => s.classList.remove('active1'))
+//   this.classList.add('active1')
+
+// asideList.forEach( (s) => s.addEventListener("click", function (e) {
+//     document.querySelector(".body").classList.toggle("toggle");
+// })
+// );
