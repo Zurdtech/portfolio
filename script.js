983124dd1ffae02dc9454f050b1f898bc593f940
Zurdtech <adexsade8@gmail.com>
2025-05-03 18:46:32 +0100

    editing

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  "use strict";
+"use strict";
 
 // const toggle = document
 //   .querySelector(".menu-box")
@@ -6,11 +6,13 @@
 //     document.querySelector(".body").classList.toggle("toggle");
 //   });
 
-
 const aside = document.querySelector(".toggle");
 const open = document.querySelector(".open");
 const close = document.querySelector(".close");
 const asideList = document.querySelectorAll(".name");
+const toTop = document.querySelector(".toTop");
+
+// menu start
 
 open.addEventListener("click", function (e) {
   document.body.classList.toggle("toggle");
@@ -19,18 +21,40 @@ open.addEventListener("click", function (e) {
 close.addEventListener("click", function (e) {
   document.body.classList.toggle("toggle");
 });
- 
-function asideAction(){
-  asideList.forEach(s => s.classList.remove('active2'))
-  this.classList.add('active2')
-  document.body.classList.toggle('toggle')
+
+// menu end
+
+// navList start
+
+function asideAction() {
+  asideList.forEach((s) => s.classList.remove("active2"));
+  this.classList.add("active2");
+  document.body.classList.toggle("toggle");
 }
 
-asideList.forEach(s => s.addEventListener('click', asideAction))
+asideList.forEach((s) => s.addEventListener("click", asideAction));
 
-let typed = new Typed('.movingText', {
-  strings: ['Web Developer', 'Web Designer', 'Technologist', 'Freelancer'],
-  typeSpeed: 150,
-  backSpeed: 150, 
-  loop: true
+// navList end
+
+// autoType start
+
+let typed = new Typed(".movingText", {
+  strings: ["Web Developer.", "Web Designer.", "Technologist.", "Freelancer.", 'Analyst.'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+
+// autoType end
+
+// toTop start
+
+window.addEventListener('scroll', (s) => {
+  if(window.pageYOffset > 100){
+    toTop.classList.add('active')
+  } else{
+    toTop.classList.remove('active')
+  }
 })
+
+// toTop end
